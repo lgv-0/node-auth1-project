d31ac42305863e35aae1f7835787510625dda484
Logan Van Hook <loganvan@att.net>
2020-03-24 13:18:34 -0400

    Setup jwt

--- middleware/restricted.js
+++ middleware/restricted.js
@@ -1,7 +1,25 @@
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
+
 module.exports = (req, res, next) =>
 {
-    if (req.session && req.session.user)
-        next();
+    const { authorization } = req.headers;
+
+    let publicKEY = fs.readFileSync(require("path").join(__dirname, "../keys/rs512.key.pub"), 'utf8');
+
+    if (authorization)
+    {
+        jwt.verify(authorization, publicKEY, {algorithms:["RS512"]}, (err, decodedToken)=>
+        {
+            if (!err)
+            {
+                console.log(decodedToken);
+                next();
+            }
+            else
+                res.status(401).send("You shall not pass!");
+        });
+    }
     else
-        res.status(401).send("You shall not pass!");
+        res.status(400).send("NO creds");
 }
--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express();
 const Crypt = require("bcryptjs");
 const DBHandle = require("../data/dbhandle");
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
 
 router.post("/", (req, res)=>
 {
@@ -15,12 +17,9 @@ router.post("/", (req, res)=>
         
         if (Crypt.compareSync(req.body.password, response.password))
         {
-            req.session.user =
-            {
-                id:response.id,
-                username:response.username
-            }
-            res.status(200).send("Good!");
+            const token = generateToken(response);
+
+            res.status(200).json({msg:"ok!", token});
         }
         else
             res.status(404).send("Invalid credentials");
@@ -30,5 +29,23 @@ router.post("/", (req, res)=>
         res.status(500).send("Internal server error");
     })
 });
+
+function generateToken(user)
+{
+    const payload =
+    {
+        username: user.username
+    }
+
+    let privateKEY  = fs.readFileSync(require("path").join(__dirname, "../keys/rs512.key"), 'utf8');
+
+    const options =
+    {
+        algorithm: "RS512",
+        expiresIn: "1h"
+    }
+
+    return jwt.sign(payload, {key:privateKEY, passphrase:"secret"}, options);
+}
 
 module.exports = router;
